238183edc659f206a4f513a903067f06dfdca38f
Larissa Bileski <99213862+larissabileski@users.noreply.github.com>
2022-09-29 08:39:25 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import Navbar from "./components/Navbar.vue";
+import Card from "./components/Card.vue"
+export default {
+  components: { Navbar, Card},
+};
+</script>
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </main>
+</template>
+
+<style>
+body {
+  background-color: #000;
+}
+header {
+  height: 15%;
+  width: 100%;
+
+}
+main {
+  height: 70%;
+  display: flex;
+  padding: 50px;
+  flex-wrap: wrap;
+}
+</style>
